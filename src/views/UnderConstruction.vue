e0e8295bab40e99069ae900ff689718e9cac6970
Mahad Farooq <mac@macs-MacBook-Pro.local>
2022-08-12 01:38:14 +0500

    project setup.

---
+++ src/views/UnderConstruction.vue
@@ -0,0 +1,13 @@
+<template>
+  <body class="image h-screen bg-center bg-no-repeat bg-cover">
+    <h1 class="font-mono">We are under construction</h1>
+  </body>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.image {
+  background-image: url("../assets/background.jpg");
+}
+</style>
