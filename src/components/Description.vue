c57cda8d7cbcc799353d025b246aa90c1cf59e81
Mahad Farooq <mac@macs-MacBook-Pro.local>
2022-08-14 02:41:49 +0500

    Under construction page

---
+++ src/components/Description.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex flex-col h-1/2 justify-center pl-4">
+    <p class="font-mono font-bold text-6xl">Hi.</p>
+    <p class="font-mono font-bold text-4xl">I'm Mahad</p>
+    <p class="font-mono font-bold text-2xl">A Software Engineer</p>
+    <p class="font-mono font-bold text-black w-3/4">
+      This is an under construction personal portfolio.
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "Description",
+  data() {
+    return {};
+  },
+  methods: {},
+});
+</script>
+
+<style scoped></style>
--- src/views/UnderConstruction.vue
+++ src/views/UnderConstruction.vue
@@ -1,13 +1,57 @@
 <template>
   <body class="image h-screen bg-center bg-no-repeat bg-cover">
-    <h1 class="font-mono">We are under construction</h1>
+    <li class="list-none flex items-center gap-5 p-4">
+      <ul>
+        <a href="https://github.com/Mahadfarooq10">
+          <button>
+            <Github :width="2.5" :height="2.5" class="enlarge" />
+          </button>
+        </a>
+      </ul>
+      <ul>
+        <a href="https://www.linkedin.com/in/mahad-farooq-2004a71b3/">
+          <button>
+            <Linkedin :width="2.5" :height="2.5" class="enlarge" />
+          </button>
+        </a>
+      </ul>
+      <ul>
+        <a href="mailto: mahadfarooq1998@gmail.com">
+          <button>
+            <At :width="2.8" :height="2.8" class="enlarge" />
+          </button>
+        </a>
+      </ul>
+      <ul>
+        <a href="resume.pdf" download="resume.pdf">
+          <button>
+            <Resume :width="2.3" :height="2.3" class="enlarge" />
+          </button>
+        </a>
+      </ul>
+    </li>
+    <Description />
   </body>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Github from "../components/icons/github.vue";
+import Linkedin from "../components/icons/linkedin.vue";
+import At from "../components/icons/at.vue";
+import Resume from "../components/icons/resume.vue";
+import Description from "../components/Description.vue";
+
+</script>
 
 <style scoped>
 .image {
   background-image: url("../assets/background.jpg");
+}
+.enlarge:hover {
+  transform: scale(1.25);
+}
+.enlarge {
+  transition: all 0.5s;
+  transform-origin: 50% 50%;
 }
 </style>
